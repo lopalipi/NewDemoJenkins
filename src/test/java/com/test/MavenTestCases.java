6b9d49aa3784907ed77b6c44987aec63d16de718
lopalipi <lopa.pattanaik12@gmail.com>
2020-02-10 13:38:49 +0530

    intial commit

---
+++ src/test/java/com/test/MavenTestCases.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class MavenTestCases {
+	
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("Welcome to testing");
+	}
+
+}
---
+++ src/test/java/com/test/OneTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class OneTest {
+
+	@Test
+	public void method2()
+	{	
+	System.out.println("Second Test");
+	}
+}
+
+//https://github.com/lopalipi/JenkinMavenDemo.git
